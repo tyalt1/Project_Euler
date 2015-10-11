47d8a9802feec243184de9bf7fdb2e1ffbe8fce7
Tyler Alterio <tyalt1@gmail.com>
2015-10-10 20:54:35 -0400

    made testing more concise

--- test/project_euler/test/core.clj
+++ test/project_euler/test/core.clj
@@ -1,18 +1,18 @@
 (ns project-euler.test.core
   (:use [project-euler.solutions]
-        [clojure.test :only (deftest testing is)]))
+        [clojure.test :only (deftest are)]))
 
 (deftest all-solutions
-  (testing "All Project Euler Solutions"
-    (is (= (pe001) 233168))
-    (is (= (pe002) 4613732))
-    (is (= (pe003) 6857))
-    (is (= (pe004) 906609))
-    (is (= (pe005) 232792560))
-    (is (= (pe006) 25164150))
-    (is (= (pe007) 104743))
-    (is (= (pe008) 23514624000))
-    (is (= (pe009) 31875000))
-    (is (= (pe010) 142913828922))
-    (is (= (pe011) 70600674))
-    ))
+  (are [f n] (= (f) n)
+       pe001 233168
+       pe002 4613732
+       pe003 6857
+       pe004 906609
+       pe005 232792560
+       pe006 25164150
+       pe007 104743
+       pe008 23514624000
+       pe009 31875000
+       pe010 142913828922
+       pe011 70600674
+       ))
