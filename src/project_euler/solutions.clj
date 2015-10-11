d693906e9f4789d3d780f77d6f521eee3647dc48
Tyler Alterio <tyalt1@gmail.com>
2015-10-11 17:13:05 -0400

    added problem 18 and test

--- src/project_euler/solutions.clj
+++ src/project_euler/solutions.clj
@@ -297,3 +297,28 @@
                               )
            oneThousand 11]
        (+ oneToNintynine oneHundredTo999 oneThousand)))
+
+(defn- merge-rows
+  "Helper functions for pe018 and pe067."
+  [child parent] (map + parent (map #(apply max %) (partition 2 1 child))))
+
+(defn pe018
+  "Find the maximum sum from the top of a given triangle to the bottom."
+  [] (let [tri [[75]
+                [95 64]
+                [17 47 82]
+                [18 35 87 10]
+                [20  4 82 47 65]
+                [19  1 23 75  3 34]
+                [88  2 77 73  7 63 67]
+                [99 65  4 28  6 16 70 92]
+                [41 41 26 56 83 40 80 70 33]
+                [41 48 72 33 47 32 37 16 94 29]
+                [53 71 44 65 25 43 91 52 97 51 14]
+                [70 11 33 28 77 73 17 78 39 68 17 57]
+                [91 71 52 38 17 14 91 43 58 50 27 29 48]
+                [63 66  4 68 89 53 67 30 73 16 69 87 40 31]
+                [ 4 62 98 27 23  9 70 98 73 93 38 53 60  4 23]]]
+       (->> (reverse tri)
+         (reduce merge-rows)
+         (first))))
