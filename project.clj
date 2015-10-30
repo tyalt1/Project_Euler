97c1c741decb6439b13ab5a31abef033b24db23f
Tyler Alterio <tyalt1@gmail.com>
2015-10-30 10:40:52 -0400

    added license metadate to project

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/math.combinatorics "0.1.1"]]
+  :license {:name "Eclipse Public License v1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :main project-euler.core)
