e41135e9853ad9169a0696da403351d44f287eeb
Tyler Alterio <tyalt1@gmail.com>
2015-11-06 17:04:15 -0500

    cleanup of main

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -2,18 +2,16 @@
   (:require project-euler.solutions))
 
 (defn run-problems [& problem_numbers]
-  (in-ns 'project-euler.solutions)
-  (letfn [(solution [n] (try (eval (symbol (format "pe%03d" n)))
-                          (catch RuntimeException e (fn [] "no solution"))))
-          (run-solution [n] (time ((solution n))))
-          (print-begin [] (println (apply str (repeat 14 "="))
-                                   "BEGIN"
-                                   (apply str (repeat 14 "="))))
-          (print-break [] (println (apply str (repeat 35 "="))))]
-    (do (print-begin)
-      (doseq [n problem_numbers]
-        (println "Project Euler" n ":" (run-solution n))
-        (print-break)))))
+  (letfn [(solution [n] (try
+                          (eval (symbol (format "project-euler.solutions/pe%03d"
+                                                n)))
+                          (catch RuntimeException e (fn [] "no solution"))))]
+    (println (apply str (repeat 14 "="))
+             "BEGIN"
+             (apply str (repeat 14 "=")))
+    (doseq [n problem_numbers]
+      (println "Project Euler" n ":" (time ((solution n))))
+      (println (apply str (repeat 35 "="))))))
 
 (defn -main
   "Solution Runner"
